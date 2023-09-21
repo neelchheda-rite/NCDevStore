5a7e55f2b36f190caa2be7b67847de33a461cdf1
Neel Chheda <130147345+neelchheda-rite@users.noreply.github.com>
2023-09-21 16:24:44 +0530

    Header Layout

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,8 +12,17 @@ import { Cart } from "../Cart/Cart";
 import { Context } from "../../utils/context";
 
 const Header = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) setScrolled(true);
+    else setScrolled(false);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll",handleScroll);
+  },[]);
   return (
-    <header className="main-header"> 
+    <header className={`main-header ${scrolled? 'sticky-header':''}`}>
       <div className="header-content">
         <ul className="left">
           <li>Home</li>
@@ -26,15 +35,12 @@ const Header = () => {
           <AiOutlineHeart />
           <span className="cart-icon">
             <CgShoppingCart />
-            <span>
-                5
-            </span>
+            <span>5</span>
           </span>
         </div>
       </div>
     </header>
   );
-
 };
 
 export default Header;
