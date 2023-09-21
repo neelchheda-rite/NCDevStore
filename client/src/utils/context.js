5672568b2cc126b378b32dce73e31bcdca27ca11
Neel Chheda <130147345+neelchheda-rite@users.noreply.github.com>
2023-09-21 15:26:18 +0530

    Header Component

--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 import { createContext,useState } from "react";
-=======
-import { createContext } from "react";
->>>>>>> e28b4ee473f97c01b3b3afecd46504bb05bb4f46
+
 
 export const Context  = createContext();
 
