842e68df471e3058dac11455d7ec7979b2c9936f
Neel Chheda <130147345+neelchheda-rite@users.noreply.github.com>
2023-09-21 13:30:17 +0530

    Header Component

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import "./Header.scss";
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+
+import { Search } from "./Search/Search";
+import { Cart } from "../Cart/Cart";
+import { Context } from "../../utils/context";
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className="center">GADA ELECTRONICS</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>
+                5
+            </span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
